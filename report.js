a0ab95abd27c480d6c814f96c6badfea09de9156
naresubr <nagendre@worksap.co.jp>
2019-08-30 16:54:25 +0530

    refs #loopback-test commit new end point /reports/recentreports

---
+++ report.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(Report) {
+	require('../../server/hook-creator-mods')(Report);
+	require('../../server/hook-delete-mods')(Report);
+	require('../../server/hook-find-mods')(Report);
+	require('../../server/hook-file-metadata-mods')(Report);
+	require('../../server/hook-distance-mods')(Report);
+	require('../../server/hook-thumbnail-mods')(Report);
+	require('../../server/hook-thumbnails-mods')(Report);
+	require('../../server/hook-violation-mods')(Report);
+	require('../../server/remotemethod-public')(Report);
+
+	Report.getRecentReports = async () => {
+		var response = await Report.find({ limit: 10 });
+		return response.length;
+	};
+	
+	Report.remoteMethod(
+			'getRecentReports', {
+				http: {
+					path: '/recentreports',
+					verb: 'get'
+				},
+				returns: {
+					arg: 'result',
+					type: 'object'
+				}
+			}
+	);
+	
+};
